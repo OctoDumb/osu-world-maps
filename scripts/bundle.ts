201bea4caf9aa4a3d790e65a8a9117b7f4b7a2c5
OctopuSSX <vladik0701@gmail.com>
2023-10-12 18:37:22 +0300

    Add locale generation to bundle script

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -10,6 +10,18 @@ const outputPath = path.join(process.cwd(), "out");
 if(!fs.existsSync(outputPath))
   fs.mkdirSync(outputPath);
 
+const jsonPath = path.join(outputPath, "json");
+if(!fs.existsSync(jsonPath))
+  fs.mkdirSync(jsonPath);
+
+const sqlPath = path.join(outputPath, "sql");
+if(!fs.existsSync(sqlPath))
+  fs.mkdirSync(sqlPath);
+
+const localePath = path.join(outputPath, "locale");
+if(!fs.existsSync(localePath))
+  fs.mkdirSync(localePath);
+
 const args = process.argv.slice(2);
 
 (async() => {
@@ -29,26 +41,40 @@ const args = process.argv.slice(2);
 
     let [data, sql] = await country.bundle(provider);
 
+    let json: any = JSON.parse(data.toString());
+    let locale: any = {};
+    let missingNames: string[] = [];
+    for(let feature of json.features) {
+      if(!feature.properties.name)
+        missingNames.push(feature.properties.id);
+      else
+        locale[feature.properties.id] = feature.properties.name;
+    }
+
     const end = Date.now()
 
     console.log(`• 🏭 Processing finished in ${end - start}ms`);
 
-    fs.writeFileSync(path.join(outputPath, `${country.code}.json`), data);
-    fs.writeFileSync(path.join(outputPath, `${country.code}.sql`), sql);
+    fs.writeFileSync(path.join(jsonPath, `${country.code}.json`), data);
+    fs.writeFileSync(path.join(sqlPath, `${country.code}.sql`), sql);
 
     {
       let start = Date.now();
       let gzip = zlib.gzipSync(data);
       let end = Date.now();
       console.log(`• 📚 Compressed using gzip in ${end - start}ms`);
-      fs.writeFileSync(path.join(outputPath, `${country.code}.json.gz`), gzip);
+      fs.writeFileSync(path.join(jsonPath, `${country.code}.json.gz`), gzip);
     }
     {
       let start = Date.now();
       let brotli = zlib.brotliCompressSync(data);
       let end = Date.now();
       console.log(`• 📚 Compressed using brotli in ${end - start}ms`);
-      fs.writeFileSync(path.join(outputPath, `${country.code}.json.br`), brotli);
+      fs.writeFileSync(path.join(jsonPath, `${country.code}.json.br`), brotli);
     }
+
+    fs.writeFileSync(path.join(localePath, `${country.code}.json`), JSON.stringify(locale, null, "\t"));
+    if(missingNames.length)
+    console.log(`• ⚠ Missing names for ${missingNames.join(', ')}`);
   }
 })();
