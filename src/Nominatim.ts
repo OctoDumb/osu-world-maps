a699955d0aa5ef4713ec99dbcb197b4c93635c31
OctopuSSX <vladik0701@gmail.com>
2023-10-12 18:37:04 +0300

    Fix fallbacks for names

--- src/Nominatim.ts
+++ src/Nominatim.ts
@@ -16,7 +16,7 @@ export default class Nominatim {
       type: "Feature",
       geometry: this.data.geometry,
       properties: {
-        name: this.data.official_name ?? this.data.localname,
+        name: this.data.names['name:en'] ?? this.data.names['name'] ?? this.data.official_name ?? this.data.localname,
         // IDs are required for mapshaper
         // Can be handled better
         id: this.data.names["ISO3166-2"] ?? "id",
