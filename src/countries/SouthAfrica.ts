014e631656d91ea099c958e87a59334d18684a57
OctopuSSX <vladik0701@gmail.com>
2023-10-19 18:43:45 +0300

    Fix formatting

--- src/countries/SouthAfrica.ts
+++ src/countries/SouthAfrica.ts
@@ -7,14 +7,14 @@ export default class SouthAfrica extends Country {
   zoom = 6;
 
   regions = [
-      88574, // Eastern Cape
-      92417, // Free State
-      349344, // Gauteng
-      349390, // KwaZulu Natal
-      349547, // Limpopo
-      349556, // Mpumalanga
-      349519, // North West
-      86720, // Northern Cape
-      80501, // Western Cape
+    88574, // Eastern Cape
+    92417, // Free State
+    349344, // Gauteng
+    349390, // KwaZulu Natal
+    349547, // Limpopo
+    349556, // Mpumalanga
+    349519, // North West
+    86720, // Northern Cape
+    80501, // Western Cape
   ];
 }
