84dd9f3d0b5bc379acbae7ed930ddcd5ed813b7e
Darshan Singh <88923579+GyuoMi@users.noreply.github.com>
2023-10-19 17:45:37 +0200

    added South Africa (ZA) to Countries (#3)

---
+++ src/countries/SouthAfrica.ts
@@ -0,0 +1,20 @@
+import Country from "../Country";
+
+export default class SouthAfrica extends Country {
+  code = "ZA";
+
+  center: [number, number] = [-29, 24];
+  zoom = 6;
+
+  regions = [
+    88574, // Eastern Cape
+    92417, // Free State
+    349344, // Gauteng
+    349390, // KwaZulu Natal
+    349547, // Limpopo
+    349556, // Mpumalanga
+    349519, // North West
+    86720, // Northern Cape
+    80501, // Western Cape
+  ];
+}
