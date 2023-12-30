a034d81608013b3fd84435e40c437e508d986c9c
OctopuSSX <vladik0701@gmail.com>
2023-12-30 20:59:44 +0300

    Fix Slovenia IDs

--- src/countries/Slovenia.ts
+++ src/countries/Slovenia.ts
@@ -1,4 +1,5 @@
 import Country from "../Country";
+import CustomIdModifier from "../modifiers/CustomIdModifier";
 
 export default class Slovenia extends Country {
   code = "SI";
@@ -7,17 +8,17 @@ export default class Slovenia extends Country {
   zoom = 8;
 
   regions = [
-    1703918, // Upper Carniola
-    1703668, // Southeast Slovenia
-    1703687, // Savinja 
-    1703400, // Mura
-    1703402, // Lower Sava
-    1703401, // Littoral–Inner Carniola
-    541729, // Gorizia
-    541718, // Drava
-    1703387, // Coastal–Karst
-    1699230, // Central Slovenia
-    1699148, // Central Sava
-    541722, // Carinthia
+    new CustomIdModifier(1703918, "SI-SI022"), // Upper Carniola
+    new CustomIdModifier(1703668, "SI-SI017"), // Southeast Slovenia
+    new CustomIdModifier(1703687, "SI-SI014"), // Savinja 
+    new CustomIdModifier(1703400, "SI-SI011"), // Mura
+    new CustomIdModifier(1703402, "SI-SI016"), // Lower Sava
+    new CustomIdModifier(1703401, "SI-SI018"), // Littoral–Inner Carniola
+    new CustomIdModifier(541729, "SI-SI023"), // Gorizia
+    new CustomIdModifier(541718, "SI-SI012"), // Drava
+    new CustomIdModifier(1703387, "SI-SI024"), // Coastal–Karst
+    new CustomIdModifier(1699230, "SI-SI021"), // Central Slovenia
+    new CustomIdModifier(1699148, "SI-SI015"), // Central Sava
+    new CustomIdModifier(541722, "SI-SI013"), // Carinthia
   ];
 }
