e555cddcd2055ef6b63fcf4226f0f609449f5949
OctopuSSX <vladik0701@gmail.com>
2023-12-30 20:58:18 +0300

    Fix North Macedonia IDs

--- src/countries/NorthMacedonia.ts
+++ src/countries/NorthMacedonia.ts
@@ -1,4 +1,5 @@
 import Country from "../Country";
+import CustomIdModifier from "../modifiers/CustomIdModifier";
 
 export default class NorthMacedonia extends Country {
   code = "MK"
@@ -7,14 +8,14 @@ export default class NorthMacedonia extends Country {
   zoom = 8;
 
   regions = [
-    2572311, // Vardar
-    2572069, // Eastern
-    2573888, // Southwestern
-    2572240, // Southeastern
-    2572701, // Pelagonia
-    2573935, // Polog
-    2570958, // Northeastern
-    2460455, // Skopje
+    new CustomIdModifier(2572311, "MK-MK001"), // Vardar
+    new CustomIdModifier(2572069, "MK-MK002"), // Eastern
+    new CustomIdModifier(2573888, "MK-MK003"), // Southwestern
+    new CustomIdModifier(2572240, "MK-MK004"), // Southeastern
+    new CustomIdModifier(2572701, "MK-MK005"), // Pelagonia
+    new CustomIdModifier(2573935, "MK-MK006"), // Polog
+    new CustomIdModifier(2570958, "MK-MK007"), // Northeastern
+    new CustomIdModifier(2460455, "MK-MK008"), // Skopje
   ];
 }
 
