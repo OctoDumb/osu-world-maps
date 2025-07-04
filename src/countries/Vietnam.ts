7056e6b2bf70d75ff4971128b1106b460c5b0995
Octo <vladik0701@gmail.com>
2025-07-04 18:40:27 +0300

    Vietnam 2025 changes (#9)
    * First Vietnam pass
    
    * Clean up

--- src/countries/Vietnam.ts
+++ src/countries/Vietnam.ts
@@ -3,71 +3,42 @@ import Country from "../Country";
 export default class Vietnam extends Country {
   code = "VN";
 
-  center: [number, number] = [14, 105.6];
+  center: [number, number] = [15.6, 106.1];
   zoom = 6;
 
   regions = [
     1875748, // An Giang Province
-    1904296, // Bà Rịa - Vũng Tàu Province
-    1906037, // Bình Dương Province
-    1898841, // Bình Phước Province
-    1904231, // Bình Thuận Province
-    1889794, // Bình Định Province
-    1873533, // Bạc Liêu Province
-    1902941, // Bắc Giang Province
-    1903471, // Bắc Kạn Province
     1902690, // Bắc Ninh province
-    1875968, // Bến Tre Province
     1844412, // Cao Bằng Province
     1873490, // Cà Mau Province
     1874283, // Cần Thơ
     1884018, // Gia Lai Province
-    1902973, // Hoà Bình province
-    1903478, // Hà Giang Province
-    1901010, // Hà Nam Province
     1903516, // Hà Nội
     1898458, // Hà Tĩnh Province
     1901032, // Hưng Yên Province
-    1902686, // Hải Dương Province
     1902682, // Hải Phòng
-    1874249, // Hậu Giang Province
     1973756, // Hồ Chí Minh City
     1887959, // Khánh Hòa Province
-    1874471, // Kiên Giang Province
-    1879515, // Kon Tum Province
     1903322, // Lai Châu Province
-    1877236, // Long An Province
     1903400, // Lào Cai Province
     1885367, // Lâm Đồng Province
     5522596, // Lạng Sơn Province
-    1901008, // Nam Định Province
     1898509, // Nghệ An Province
     1900963, // Ninh Bình Province
-    1886159, // Ninh Thuận Province
     1902930, // Phú Thọ Province
-    1889204, // Phú Yên Province
-    1896050, // Quảng Bình Province
-    1891352, // Quảng Nam Province
     1890793, // Quảng Ngãi Province
     1902947, // Quảng Ninh Province
     1895630, // Quảng Trị Province
-    1873632, // Sóc Trăng Province
     1903291, // Sơn La Province
     1898590, // Thanh Hoá Province
-    1901019, // Thái Bình Province
     1902967, // Thái Nguyên Province
-    1891483, // Thừa Thiên Huế Province
-    1876011, // Tiền Giang Province
-    1873642, // Trà Vinh Province
+    1891483, // Huế
     1903418, // Tuyên Quang Province
     1898961, // Tây Ninh Province
     1875887, // Vĩnh Long Province
-    1902889, // Vĩnh Phúc Province
-    1903199, // Yên Bái Province
     1903340, // Điện Biên Province
     1891418, // Đà Nẵng
     1884034, // Đăk Lăk Province
-    1884042, // Đăk Nông Province
     1904421, // Đồng Nai Province
     1875866, // Đồng Tháp Province
   ];
